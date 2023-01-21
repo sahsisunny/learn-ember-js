ba9d3bf8ff4538a30d4339795722a34790f36a87
SahsiSunny <sahsisunny@gmail.com>
2023-01-21 07:09:48 +0530

    name change

---
+++ super-rental/tests/acceptance/super-rentals-test.js
@@ -0,0 +1,39 @@
+import { module, test } from 'qunit';
+import { click, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'super-rentals/tests/helpers';
+
+module('Acceptance | super rentals', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function (assert) {
+    await visit('/');
+
+    assert.strictEqual(currentURL(), '/');
+    assert.dom('h2').hasText('Welcome to Super Rentals!');
+
+    assert.dom('.jumbo a.button').hasText('About Us');
+    await click('.jumbo a.button');
+
+    assert.strictEqual(currentURL(), '/about');
+  });
+
+  test('visiting /about', async function (assert) {
+    await visit('/about');
+    assert.dom('h2').hasText('About Super Rentals');
+
+    assert.dom('.jumbo a.button').hasText('Contact Us');
+    await click('.jumbo a.button');
+
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+  });
+
+  test('visiting /contact', async function (assert) {
+    await visit('/getting-in-touch');
+
+    assert.dom('h2').hasText('Contact Us');
+    assert.dom('a.button').hasText('About');
+
+    await click('a.button');
+
+  });
+});
